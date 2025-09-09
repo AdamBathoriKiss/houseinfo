29e02a53704308a1098325299eb5c9cce7092ae0
bkadam <adambathorikiss@gmail.com>
2025-09-09 11:42:48 +0200

    c:[fix]

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -4,10 +4,15 @@ import MainPage from "~/components/MainPage";
 import useMain from "~/hooks/useMain";
 
 export default function Main() {
-  const {houses} = useMain();
-  return (
-  <>
-  <Header houses={houses} />
-  {houses && houses.length > 0 ? <MainPage /> : <EmptyMainPage />}
-  </>)
+	const { houses } = useMain();
+
+	return (
+		<div className="h-screen flex flex-col overflow-hidden">
+			{/* Header - fix magasság */}
+			<Header houses={houses} />
+
+			{/* Main content - maradék hely */}
+			<div className="flex-1 overflow-auto">{houses && houses.length > 0 ? <MainPage /> : <EmptyMainPage />}</div>
+		</div>
+	);
 }
