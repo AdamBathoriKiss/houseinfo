024b60da6d71114bb7e6c70284c8927be76e2365
AdamBathoriKiss <adambathorikiss@gmail.com>
2025-10-07 09:05:09 +0200

    c:[fix] - DoughnutChart label issue fixed

--- app/components/DoughnutChart.tsx
+++ app/components/DoughnutChart.tsx
@@ -1,31 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
 
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'primereact/chart';
-
-export default function DoughnutChart({title}: {title: string}) {
+export default function DoughnutChart({ title }: { title: string }) {
     const [chartData, setChartData] = useState({});
     const [chartOptions, setChartOptions] = useState({});
 
     useEffect(() => {
         const documentStyle = getComputedStyle(document.documentElement);
         const data = {
-            labels: ['Aktív', 'Szabad'],
+            labels: ["Aktív", "Szabad"],
             datasets: [
                 {
                     data: [150, 100],
                     backgroundColor: [
-                        documentStyle.getPropertyValue('--blue-500'), 
-                        documentStyle.getPropertyValue('--green-500')
+                        documentStyle.getPropertyValue("--blue-500"),
+                        documentStyle.getPropertyValue("--green-500"),
                     ],
                     hoverBackgroundColor: [
-                        documentStyle.getPropertyValue('--blue-400'), 
-                        documentStyle.getPropertyValue('--green-400')
-                    ]
-                }
-            ]
+                        documentStyle.getPropertyValue("--blue-400"),
+                        documentStyle.getPropertyValue("--green-400"),
+                    ],
+                },
+            ],
         };
         const options = {
-            cutout: '70%'
+            cutout: "70%",
+            plugins: {
+                legend: {
+                    display: false, // legend elrejtése
+                },
+                tooltip: {
+                    enabled: true,
+                    callbacks: {
+                        label: function (context:any) {
+                            const value = context.parsed || 0;
+                            return `${value}`;
+                        },
+                    },
+                },
+            },
         };
 
         setChartData(data);
@@ -35,8 +48,12 @@ export default function DoughnutChart({title}: {title: string}) {
     return (
         <div className="flex flex-col h-full justify-center items-center">
             <h6>{title}</h6>
-            <Chart type="doughnut" data={chartData} options={chartOptions} className="w-35 flex flex-col items-center justify-between" />
+            <Chart
+                type="doughnut"
+                data={chartData}
+                options={chartOptions}
+                className="w-35 flex flex-col items-center justify-between"
+            />
         </div>
-    )
+    );
 }
-        
