b4038fba616975feb533f578898e1f25fca0aeb3
bkadam <adambathorikiss@gmail.com>
2025-12-01 17:59:44 +0100

    datatable fix

--- app/utils/DataHeader.tsx
+++ app/utils/DataHeader.tsx
@@ -30,11 +30,71 @@ const header = ({
 }: HeaderInterface) => {
 	const { user } = useAuth();
 	return (
-		<div className="w-full flex flex-row justify-between items-center">
+		<div className="w-full flex flex-col md:flex-row gap-3 md:gap-0 md:justify-between md:items-center">
 			<p className="mx-4 text-dark-200 font-black">{title}</p>
-			<div className="flex flex-row items-center justify-around">
-				{filter && <SearchBar filterFunction={filter} />}
+			<div className="flex flex-row items-center gap-2 px-4 md:px-0">
+				{filter && (
+					<div className="flex-1 md:flex-none">
+						<SearchBar filterFunction={filter} />
+					</div>
+				)}
 
+				<div className="flex flex-row items-center gap-2 flex-shrink-0">
+					{type === "news" && setCreateNews && (
+						<Button
+							icon="pi pi-plus"
+							tooltip="Új hír létrehozása"
+							onClick={() => setCreateNews(true)}
+							className="!text-indigo-300 !bg-transparent hover:!bg-gray-600/30"
+						/>
+					)}
+
+					{type === "maintence" && setCreateTask && (
+						<Button
+							icon="pi pi-plus"
+							tooltip="Új feladat létrehozása"
+							onClick={() => setCreateTask(true)}
+							className="!text-indigo-300 !bg-transparent hover:!bg-gray-600/30"
+						/>
+					)}
+
+					{type === "parking" && setCreateParking && (
+						<Button
+							icon="pi pi-plus"
+							tooltip="Új feladat létrehozása"
+							onClick={() => setCreateParking(true)}
+							className="!text-indigo-300 !bg-transparent hover:!bg-gray-600/30"
+						/>
+					)}
+					
+					{setOnMaximizedOpened !== undefined && (
+						<div className="hidden md:block">
+							<span
+								className="pi pi-window-maximize cursor-pointer hover:text-blue-500"
+								onClick={() => setOnMaximizedOpened(!onMaximizedOpened)}
+							/>
+						</div>
+					)}
+
+					{fileUpdateDialog && (
+						<FileUploader buildingId={buildingId ? buildingId : null} uploadedBy={user.userId} />
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const headerMaximalized = ({ type, filter, setCreateNews, setCreateTask }: HeaderInterface) => {
+	return (
+		<div className="flex flex-col md:flex-row items-stretch md:items-center gap-3 md:gap-2">
+			{filter && (
+				<div className="flex-1">
+					<SearchBar filterFunction={filter} />
+				</div>
+			)}
+
+			<div className="flex flex-row gap-2 justify-end">
 				{type === "news" && setCreateNews && (
 					<Button
 						icon="pi pi-plus"
@@ -52,54 +112,7 @@ const header = ({
 						className="!text-indigo-300 !bg-transparent hover:!bg-gray-600/30"
 					/>
 				)}
-
-				{type === "parking" && setCreateParking && (
-					<Button
-						icon="pi pi-plus"
-						tooltip="Új feladat létrehozása"
-						onClick={() => setCreateParking(true)}
-						className="!text-indigo-300 !bg-transparent hover:!bg-gray-600/30"
-					/>
-				)}
-				{setOnMaximizedOpened !== undefined && (
-					<div className="hidden md:block">
-						<span
-							className="pi pi-window-maximize cursor-pointer hover:text-blue-500"
-							onClick={() => setOnMaximizedOpened(!onMaximizedOpened)}
-						/>
-					</div>
-				)}
-
-				{fileUpdateDialog && (
-					<FileUploader buildingId={buildingId ? buildingId : null} uploadedBy={user.userId} />
-				)}
 			</div>
-		</div>
-	);
-};
-
-const headerMaximalized = ({ type, filter, setCreateNews, setCreateTask }: HeaderInterface) => {
-	return (
-		<div className="flex flex-row items-center justify-between gap-2">
-			{filter && <SearchBar filterFunction={filter} />}
-
-			{type === "news" && setCreateNews && (
-				<Button
-					icon="pi pi-plus"
-					tooltip="Új hír létrehozása"
-					onClick={() => setCreateNews(true)}
-					className="!text-indigo-300 !bg-transparent hover:!bg-gray-600/30"
-				/>
-			)}
-
-			{type === "maintence" && setCreateTask && (
-				<Button
-					icon="pi pi-plus"
-					tooltip="Új feladat létrehozása"
-					onClick={() => setCreateTask(true)}
-					className="!text-indigo-300 !bg-transparent hover:!bg-gray-600/30"
-				/>
-			)}
 		</div>
 	);
 };
--- app/utils/SearchBar.tsx
+++ app/utils/SearchBar.tsx
@@ -8,11 +8,11 @@ interface Filter {
 
 export default function SearchBar({ filterFunction }: Filter) {
 	return (
-		<div className="flex items-center gap-3">
-			<IconField iconPosition="left" className="w-full">
+		<div className="flex items-center gap-3 w-full md:w-auto">
+			<IconField iconPosition="left" className="w-full md:w-auto">
 				<InputIcon className="pi pi-search" />
 				<InputText
-					className="mx-4 !bg-transparent md:w-15 lg:w-15 h-[2.5rem] !rounded-4xl"
+					className="w-full md:w-64 lg:w-80 h-[2.5rem] !bg-transparent !rounded-full px-4"
 					placeholder="Search"
 					onChange={(e) => filterFunction(e.target.value)}
 				/>
@@ -20,4 +20,3 @@ export default function SearchBar({ filterFunction }: Filter) {
 		</div>
 	);
 }
-
